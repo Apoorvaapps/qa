599d2a5884fadec645641f45e2b38b8fbd540b1f
Apoorva Singh <Apoorva.Singh@nectechnologies.in>
2023-07-17 00:14:08 +0530

    modify

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ provider "azurerm" {
 
 # resource group
 resource "azurerm_resource_group" "default" {
-  location = var.resource_group_location
-  name     = var.resource_group_name
+  resource_group_location = var.resource_group_location
+  resource_group_name     = var.resource_group_name
   #location = "centralindia"
   #name     = "rg-terraformcloudtest"
 }
